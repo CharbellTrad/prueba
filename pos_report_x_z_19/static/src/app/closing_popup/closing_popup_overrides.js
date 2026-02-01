4bd96e654b7b973abc32ce206783da8fb5f2eebe
Charbel Trad <charbeltrad546@gmail.com>
2026-02-01 00:15:56 -0400

    Modulo de Odoo19

---
+++ pos_report_x_z_19/static/src/app/closing_popup/closing_popup_overrides.js
@@ -0,0 +1,25 @@
+/** @odoo-module */
+
+import { ClosePosPopup } from "@point_of_sale/app/components/popups/closing_popup/closing_popup";
+import { patch } from "@web/core/utils/patch";
+import { useService } from "@web/core/utils/hooks";
+
+patch(ClosePosPopup.prototype, {
+    setup() {
+        super.setup();
+        this.orm = useService("orm");
+        this.action = useService("action");
+    },
+
+    async printReportZ() {
+        try {
+            const result = await this.orm.call("pos.report.xz", "generate_report_from_pos", [
+                this.pos.config.id,
+                "z",
+            ]);
+            await this.action.doAction(result);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+});
---
+++ pos_report_x_z_19/static/src/app/navbar/navbar_overrides.js
@@ -0,0 +1,25 @@
+/** @odoo-module */
+
+import { Navbar } from "@point_of_sale/app/components/navbar/navbar";
+import { patch } from "@web/core/utils/patch";
+import { useService } from "@web/core/utils/hooks";
+
+patch(Navbar.prototype, {
+    setup() {
+        super.setup();
+        this.orm = useService("orm");
+        this.action = useService("action");
+    },
+
+    async printReportX() {
+        try {
+            const result = await this.orm.call("pos.report.xz", "generate_report_from_pos", [
+                this.pos.config.id,
+                "x",
+            ]);
+            await this.action.doAction(result);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+});
